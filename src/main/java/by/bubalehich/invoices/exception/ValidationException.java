67bda2fcc747545e57853ff28240639f0085e745
Bubalehich <janeridebmx@gmail.com>
2023-02-18 00:51:06 +0300

    fix: fix eception handler

--- src/main/java/by/bubalehich/invoices/exception/ValidationException.java
+++ src/main/java/by/bubalehich/invoices/exception/ValidationException.java
@@ -1,6 +1,5 @@
 package by.bubalehich.invoices.exception;
 
-//@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ValidationException extends RuntimeException {
     public ValidationException(String message) {
         super(message);
--- src/main/java/by/bubalehich/invoices/exception/handler/CommonExceptionHandler.java
+++ src/main/java/by/bubalehich/invoices/exception/handler/CommonExceptionHandler.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
